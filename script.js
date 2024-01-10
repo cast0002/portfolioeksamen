670e9ab8db58d4b1c5c85756d0eeeb222337ddd8
cast0002 <143484305+cast0002@users.noreply.github.com>
2024-01-10 02:50:58 +0100

    done

--- script.js
+++ script.js
@@ -73,7 +73,7 @@
         document.getElementById("image4").addEventListener("dblclick", doubleClick);
         document.getElementById("image5").addEventListener("dblclick", doubleClick);
     
- /*scroll*/
+ /*scroll knap*/
 
         function scrollToBottom() {
             window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
@@ -93,37 +93,3 @@
                 behavior: 'smooth'
             });
         }
-/*om mig*/
-const popImage = document.getElementById("popImage");
-
-function handleScroll() {
-  const scrollPosition = window.scrollY;
-  const imagePosition = popImage.offsetTop;
-
-  if (scrollPosition > imagePosition - window.innerHeight / 2) {
-    popImage.style.opacity = 1;
-    popImage.classList.add("pop-animation");
-  }
-}
-
-window.addEventListener("scroll", handleScroll);
-
-const popImage2 = document.getElementById("popImage2");
-
-function handleScroll2() {
-  const scrollPosition = window.scrollY;
-  const imagePosition = popImage2.offsetTop;
-
-  // Tidsforsinkelse på 500 ms (0.5 sekunder)
-  const delay = 1000;
-
-  if (scrollPosition > imagePosition - window.innerHeight / 2) {
-    // Brug setTimeout til at forsinke visningen af det andet billede
-    setTimeout(() => {
-      popImage2.style.opacity = 1;
-      popImage2.classList.add("pop-animation");
-    }, delay);
-  }
-}
-
-window.addEventListener("scroll", handleScroll2);
